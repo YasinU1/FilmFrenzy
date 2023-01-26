58f3bd5600537d6d79d80e1207a124b0f41b2da2
YasinU1 <sayedmuhammadyasin.usman@vodafone.com>
2023-01-26 12:24:10 +0000

    added mongo functionality

--- server/server.js
+++ server/server.js
@@ -6,9 +6,40 @@ const app = express()
 const films = {
     "users": ["Avatar", "Avenger:Endgame", "Bullet Train", "Dune", "Transformers: Dark of the Moon"]
 }
+app.listen(port, () => {
+    console.log("Server started on port 5000") })
 
-app.get('/api', (request, response)=>{
-    response.json(films)
-})
 
-app.listen(port, () => { console.log("Server started on port 5000") })
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Access-Control-Request-Headers", "*");
+myHeaders.append("api-key", "2pLVyVoLai1QVk1AZwfAC1UvcueRt3mX9ha7J3SbQxIvbwmWij3WJgGqMlkd3Ix2");
+myHeaders.append("Accept", "application/json");
+
+var raw = JSON.stringify({
+  "dataSource": "Cluster0",
+  "database": "Films",
+  "collection": "films",
+  "filter": {}
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+app.get('/api', (request, response) => {
+    fetch("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            console.log('error', error);
+            response.status(500).send('Error: ' + error);
+        });
+});
+
+      
