b585b408495c63a4d26c45bf6dfdb80cd1f6e9b4
YasinU1 <sayedmuhammadyasin.usman@vodafone.com>
2023-02-01 23:59:02 +0000

    feature: added customer functionaility with our mongodb database so it can pull users from there

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,6 +16,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const isomorphic_fetch_1 = __importDefault(require("isomorphic-fetch"));
+const axios_1 = __importDefault(require("axios"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -16,7 +26,7 @@ myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Access-Control-Request-Headers", "*");
 myHeaders.append("api-key", "2pLVyVoLai1QVk1AZwfAC1UvcueRt3mX9ha7J3SbQxIvbwmWij3WJgGqMlkd3Ix2");
 myHeaders.append("Accept", "application/json");
-const raw = JSON.stringify({
+const rawFilms = JSON.stringify({
     "dataSource": "Cluster0",
     "database": "Films",
     "collection": "films",
@@ -25,11 +35,34 @@ const raw = JSON.stringify({
 const requestOptions = {
     method: 'POST',
     headers: myHeaders,
-    body: raw,
+    body: rawFilms,
+    redirect: 'follow'
+};
+const rawCustomers = JSON.stringify({
+    "dataSource": "Cluster0",
+    "database": "Films",
+    "collection": "customers",
+    "filter": {}
+});
+const requestOptionsCustomers = {
+    method: 'POST',
+    headers: myHeaders,
+    body: rawCustomers,
     redirect: 'follow'
 };
 app.get('/api', (req, res) => {
     (0, isomorphic_fetch_1.default)("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptions)
+        .then(res => res.json())
+        .then(data => {
+        res.json(data);
+    })
+        .catch(error => {
+        console.log('error', error);
+        res.status(500).send('Error: ' + error);
+    });
+});
+app.get('/users', (req, res) => {
+    (0, isomorphic_fetch_1.default)("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptionsCustomers)
         .then(res => res.json())
         .then(data => {
         res.json(data);
@@ -46,25 +79,25 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-// Array of example users for testing purposes
-const users = [
-    {
-        id: 1,
-        name: 'Maria Doe',
-        email: 'maria@example.com',
-        password: 'maria123'
-    },
-    {
-        id: 2,
-        name: 'Juan Doe',
-        email: 'juan@example.com',
-        password: 'juan123'
-    }
-];
+let customerArray = [];
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield axios_1.default.get('http://localhost:8000/users');
+        customerArray = res.data.documents.map((customer) => {
+            return {
+                _id: customer._id,
+                name: customer.fname + " " + customer.sname,
+                email: customer.email,
+                password: customer.password
+            };
+        });
+    });
+}
+fetchData();
 // route login
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
-    const user = users.find(user => {
+    const user = customerArray.find(user => {
         return user.email === email && user.password === password;
     });
     if (!user) {
@@ -72,3 +105,18 @@ app.post('/login', (req, res) => {
     }
     return res.status(200).json(user);
 });
+//   {
+//     "documents": [
+//       {
+//       "_id":"63d26638277f5d6c986844f2",
+//       "fname":"Olivia",
+//       "sname":"McAndrew",
+//       "DOB":"100999",
+//       "email":"olivia.mca@gmail.com",
+//       "username":"livmcandrew",
+//       "password":123456
+//     }
+//   ]
+// }
+// [{"id":1,"name":"Maria Doe","email":"maria6@example.com","password":"maria123"}]
+// [{"_id":"1","name":"Olivia McAndrew","email":"olivia@example.com","password":"liv123"}]
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,8 @@ import express, { Express, Request, Response } from "express";
 import path from "path";
 import cors from "cors";
 import fetch from 'isomorphic-fetch';
+import axios from 'axios';
+
 
 dotenv.config();
 
@@ -19,7 +21,7 @@ myHeaders.append("Access-Control-Request-Headers", "*");
 myHeaders.append("api-key", "2pLVyVoLai1QVk1AZwfAC1UvcueRt3mX9ha7J3SbQxIvbwmWij3WJgGqMlkd3Ix2");
 myHeaders.append("Accept", "application/json");
 
-const raw = JSON.stringify({
+const rawFilms = JSON.stringify({
   "dataSource": "Cluster0",
   "database": "Films",
   "collection": "films",
@@ -29,7 +31,20 @@ const raw = JSON.stringify({
 const requestOptions: RequestInit = {
   method: 'POST',
   headers: myHeaders,
-  body: raw,
+  body: rawFilms,
+  redirect: 'follow'
+};
+const rawCustomers = JSON.stringify({
+  "dataSource": "Cluster0",
+  "database": "Films",
+  "collection": "customers",
+  "filter": {}
+});
+
+const requestOptionsCustomers: RequestInit = {
+  method: 'POST',
+  headers: myHeaders,
+  body: rawCustomers,
   redirect: 'follow'
 };
 
@@ -45,6 +60,18 @@ app.get('/api', (req, res) => {
         });
 });
 
+app.get('/users', (req, res) => {
+  fetch("https://data.mongodb-api.com/app/data-afuuc/endpoint/data/v1/action/find", requestOptionsCustomers)
+      .then(res => res.json())
+      .then(data => {
+          res.json(data)
+      })
+      .catch(error => {
+          console.log('error', error);
+          res.status(500).send('Error: ' + error);
+      });
+});
+
 app.get('/', (req: Request, res: Response) => {
 res.send('<h1>Hello World From the Typescript Server!</h1>')
 });
@@ -56,40 +83,57 @@ console.log(`Example app listening on port ${port}`)
 });
 
 
+
+
+let customerArray: any[] = [];
+
+async function fetchData() {
+  const res = await axios.get('http://localhost:8000/users');
+  customerArray = res.data.documents.map((customer: {_id: string, fname: string, sname: string, email: string,password: string}) => {
+    return {
+      _id: customer._id,
+      name: customer.fname + " " + customer.sname,
+      email: customer.email,
+      password: customer.password
+    }
+  });
+}
+fetchData();
 interface FormInputs {
     email: string,
     password: string
   }
 
-  // Array of example users for testing purposes
-  const users = [
-    {
-      id: 1,
-      name: 'Maria Doe',
-      email: 'maria@example.com',
-      password: 'maria123'
-    },
-    {
-      id: 2,
-      name: 'Juan Doe',
-      email: 'juan@example.com',
-      password: 'juan123'
-    }
-  ];
+// route login
+app.post('/login', (req: Request, res: Response) => {
+  const { email, password }:FormInputs = req.body;
 
-  // route login
-  app.post('/login', (req: Request, res: Response) => {
-    const { email, password }:FormInputs = req.body;
-
-    const user = users.find(user => {
-      return user.email === email && user.password === password
-    });
-
-    if (!user) {
-      return res.status(404).send('User Not Found!')
-    }
-
-    return res.status(200).json(user)
+  const user = customerArray.find(user => {
+    return user.email === email && user.password === password
   });
 
+  if (!user) {
+    return res.status(404).send('User Not Found!')
+  }
+
+  return res.status(200).json(user)
+});
+
+
+//   {
+//     "documents": [
+  //       {
+  //       "_id":"63d26638277f5d6c986844f2",
+  //       "fname":"Olivia",
+  //       "sname":"McAndrew",
+  //       "DOB":"100999",
+  //       "email":"olivia.mca@gmail.com",
+  //       "username":"livmcandrew",
+  //       "password":123456
+  //     }
+  //   ]
+  // }
   
+
+  // [{"id":1,"name":"Maria Doe","email":"maria6@example.com","password":"maria123"}]
+  // [{"_id":"1","name":"Olivia McAndrew","email":"olivia@example.com","password":"liv123"}]
